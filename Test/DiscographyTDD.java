b7bc7b1ab8e015d372e88d8bacf5ada5ed6437ea
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-09-20 11:15:38 +0200

    testDiscographyWithOneRecord passed

--- Test/DiscographyTDD.java
+++ Test/DiscographyTDD.java
@@ -15,46 +15,47 @@ class DiscographyTDD {
         assertEquals(track.getDuration(), Duration.ofMinutes(10).plusSeconds(8));
         assertFalse(track.isBonusTrack());
     }
+
     @Test
     void testTrack2() {
         Track track2 = new Track("America", Duration.ofMinutes(4).plusSeconds(12), true);
-        assertEquals(track2.getTitle(),"America");
+        assertEquals(track2.getTitle(), "America");
         assertEquals(track2.duration, Duration.ofMinutes(4).plusSeconds(12));
         assertTrue(track2.isBonusTrack());
     }
 
     @Test
     void testEmptyRecordCreation() {
-        Record record = new Record("single","Under Production", LocalDate.of(2074,10,6));
-        assertEquals("single",record.getType());
-        assertEquals("Under production",record.getTitle());
-        assertEquals(LocalDate.of(2074,10,6),record.getReleaseDate());
-        assertEquals(0,record.getTrackCount());
-        assertEquals(Duration.ofSeconds(0),record.getPlayTime());
+        Record record = new Record("single", "Under Production", LocalDate.of(2074, 10, 6));
+        assertEquals("single", record.getType());
+        assertEquals("Under production", record.getTitle());
+        assertEquals(LocalDate.of(2074, 10, 6), record.getReleaseDate());
+        assertEquals(0, record.getTrackCount());
+        assertEquals(Duration.ofSeconds(0), record.getPlayTime());
     }
 
     @Test
     void testRecordCreationWithTwoTracks() {
-        Record record = new Record("album","Close to the Edge", LocalDate.of(1972,9,13));
-        assertEquals("album",record.getType());
-        assertEquals("Close to the Edge",record.getTitle());
-        assertEquals(LocalDate.of(1972,9,13),record.getReleaseDate());
-        assertEquals(2,record.getTrackCount());
-        assertEquals(Duration.ofMinutes(14).plusSeconds(20),record.getPlayTime());
+        Record record = new Record("album", "Close to the Edge", LocalDate.of(1972, 9, 13));
+        assertEquals("album", record.getType());
+        assertEquals("Close to the Edge", record.getTitle());
+        assertEquals(LocalDate.of(1972, 9, 13), record.getReleaseDate());
+        assertEquals(2, record.getTrackCount());
+        assertEquals(Duration.ofMinutes(14).plusSeconds(20), record.getPlayTime());
     }
 
     @Test
-    void testEmptyDiscography(){
+    void testEmptyDiscography() {
         Discography discography = new Discography("Future Artist");
         assertEquals("Future Artist", discography.getArtistName());
-        assertEquals(0,discography.getRecordCount());
+        assertEquals(0, discography.getRecordCount());
     }
 
     @Test
-    void testDiscographyWithOneRecord(){
+    void testDiscographyWithOneRecord() {
         Discography discography = new Discography("Yes");
         assertEquals("Yes", discography.getArtistName());
-        assertEquals(1,discography.getRecordCount());
+        assertEquals(1, discography.getRecordCount());
         assertEquals("Close to the Edge", discography.getAlbum());
     }
 
