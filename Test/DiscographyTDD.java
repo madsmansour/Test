ef198152896fe05c6997652396acdbd55d5f4397
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-09-20 09:26:16 +0200

    2 green test

--- Test/DiscographyTDD.java
+++ Test/DiscographyTDD.java
@@ -7,10 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiscographyTDD {
 
     @Test
-    void testTrack() {
+    void testTrack1() {
         Track track = new Track("You and I", Duration.ofMinutes(10).plusSeconds(8), false);
         assertEquals(track.getTitle(), "You and I");
         assertEquals(track.getDuration(), Duration.ofMinutes(10).plusSeconds(8));
         assertFalse(track.isBonusTrack());
+    }
+    @Test
+    void testTrack2() {
+        Track track2 = new Track("America", Duration.ofMinutes(4).plusSeconds(12), true);
+        assertEquals(track2.getTitle(),"America");
+        assertEquals(track2.duration, Duration.ofMinutes(4).plusSeconds(12));
+        assertTrue(track2.isBonusTrack());
     }
 }
